9e8c3b128f36c14ed39a0c82b0f19aa25fc6b07c
stevacca <stevacca11@gmail.com>
2019-10-31 14:46:50 +0100

    Add scripts to show sentiment and VAD metrics with Plotly

---
+++ show_VAD.py
@@ -0,0 +1,87 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+import pandas as pd
+import datetime as dt
+from plotly import tools
+import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
+import os
+from datetime import datetime
+
+def from_unix_to_datestamp(date_time_stamp):
+    """
+    Transform from unix to datestamp date
+    :param date_time_stamp: single unix format date
+    :return: datestamp date
+    """
+    date_time_stamp = int(date_time_stamp)
+    return datetime.utcfromtimestamp(date_time_stamp).strftime('%Y-%m-%d %H:%M:%S')
+
+
+if __name__ == '__main__':
+    social = 'reddit'
+    tema = 'Bitcoin'
+
+    filenames = [
+        ('Bitcoin_messages_2018_december_data.csv', '#1874CD')
+        # ('Vader_sentiment_Orbita Ferrari_cleanedData_2019-09-01_2019-09-30.csv','#1874CD')
+        # ('Vader_sentiment_Leclerc_messages_2019_september_data.csv','#1874CD'),
+        #          ('Vader_sentiment_Vettel_messages_2019_september_data.csv', '#228B22')
+        # ('Vader_sentiment_ferrari_2019_september_data.csv', '#DC143C'),
+                 # ('Vader_sentiment_formula1_2019_september_data.csv', '#3D59AB'),
+                 # ('Vader_sentiment_scuderiaferrari_2019_september_data.csv', '#228B22') # troppe poche osservazioni
+                 ]
+    n_files = len(filenames)
+
+    plotly.tools.set_credentials_file(username='stiffler93', api_key='Ospn0kRCMpCz8XwkI88g')
+    fig = tools.make_subplots(rows=3, cols=1)
+
+    for i in range(1, n_files+1):
+        print(i)
+        df = pd.read_csv(os.path.join(os.getcwd(), 'vad_metrics_folder', filenames[i-1][0]))
+        print(df.columns)
+        print(df.date)
+        # df['date'] = [from_unix_to_datestamp(ts) for ts in df['date'].tolist()]
+        df['date'] = pd.to_datetime(df['date'])
+        df['date'] = df['date'].apply(lambda x: dt.datetime.strftime(x, '%Y-%m-%d'))
+
+        df = df.set_index('date')
+        ts = df.groupby('date').mean()
+        query = filenames[i-1][0].split('_')[2]
+        print(ts)
+        # Create and style traces
+        trace1 = go.Scatter(
+            x=ts.index,
+            y=ts.valence,
+            name='Valence',
+            mode='lines+markers',
+            line=dict(
+                color=filenames[i-1][1],
+                width=4)
+        )
+        trace2 = go.Scatter(
+            x=ts.index,
+            y=ts.arousal,
+            name='Arousal',
+            mode='lines+markers',
+            line=dict(
+                color='#FF4040',
+                width=4)
+        )
+        trace3 = go.Scatter(
+            x=ts.index,
+            y=ts.dominance,
+            name='Dominance',
+            mode='lines+markers',
+            line=dict(
+                color='#FFD700',
+                width=4)
+        )
+
+        fig.append_trace(trace1, row=1, col=1)
+        fig.append_trace(trace2, row=2, col=1)
+        fig.append_trace(trace3, row=3, col=1)
+
+    fig['layout'].update(height=600, width=800, title='Andamento delle metriche VAD su Reddit per la chiave di ricerca' + tema .capitalize())
+    py.plot(fig, filename='Sentiment Analysis Reddit')
---
+++ show_sentiment.py
@@ -0,0 +1,71 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+import pandas as pd
+import datetime as dt
+from plotly import tools
+import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
+import os
+from datetime import datetime
+
+def from_unix_to_datestamp(date_time_stamp):
+    """
+    Transform from unix to datestamp date
+    :param date_time_stamp: single unix format date
+    :return: datestamp date
+    """
+    date_time_stamp = int(date_time_stamp)
+    return datetime.utcfromtimestamp(date_time_stamp).strftime('%Y-%m-%d %H:%M:%S')
+
+
+if __name__ == '__main__':
+    social = 'reddit'
+    tema = 'Bitcoin'
+
+    filenames = [
+        ('sentiment_Bitcoin_messages_2019_january_data.csv', '#1874CD')
+        # ('Vader_sentiment_Orbita Ferrari_cleanedData_2019-09-01_2019-09-30.csv','#1874CD')
+        # ('Vader_sentiment_Leclerc_messages_2019_september_data.csv','#1874CD'),
+        #          ('Vader_sentiment_Vettel_messages_2019_september_data.csv', '#228B22')
+        # ('Vader_sentiment_ferrari_2019_september_data.csv', '#DC143C'),
+                 # ('Vader_sentiment_formula1_2019_september_data.csv', '#3D59AB'),
+                 # ('Vader_sentiment_scuderiaferrari_2019_september_data.csv', '#228B22') # troppe poche osservazioni
+                 ]
+    n_files = len(filenames)
+
+    plotly.tools.set_credentials_file(username='stiffler93', api_key='Ospn0kRCMpCz8XwkI88g')
+    fig = tools.make_subplots(rows=n_files, cols=1)
+
+    for i in range(1, n_files+1):
+        print(i)
+        df = pd.read_csv(os.path.join(os.getcwd(), 'sentiment_results', social+'_sentiment', filenames[i-1][0]))
+        print(df.columns)
+        print(df.created_utc)
+        if social == 'reddit':
+            df['date'] = [from_unix_to_datestamp(ts) for ts in df['created_utc'].tolist()]
+        else:
+            df['date'] = [from_unix_to_datestamp(ts) for ts in df['date'].tolist()]
+        df['date'] = pd.to_datetime(df['date'])
+        df['date'] = df['date'].apply(lambda x: dt.datetime.strftime(x, '%Y-%m-%d'))
+
+        df = df.set_index('date')
+        ts = df.groupby('date').sentiment_code.mean()
+        query = filenames[i-1][0].split('_')[2]
+
+        # Create and style traces
+        trace = go.Scatter(
+            x=ts.index,
+            y=ts,
+            name='Sentiment ' + query.capitalize(),
+            mode='lines+markers',
+            line=dict(
+                color=filenames[i-1][1],
+                width=4)
+        )
+
+        fig.append_trace(trace, i, 1)
+
+    fig['layout'].update(height=600, width=800, title='Sentiment Analysis su Reddit per il tema ' + tema .capitalize())
+    py.plot(fig, filename='Sentiment Analysis Reddit')
+#
