8be68e0e24deafe837aa641638c0ab2092b0dd11
stevacca <stevacca11@gmail.com>
2019-11-26 10:18:59 +0100

    Add main anche grid search script to choose spam parameters

---
+++ grid_search.py
@@ -0,0 +1,104 @@
+from nltk.stem.lancaster import LancasterStemmer
+from nltk.corpus import stopwords
+from nltk.stem.porter import *
+from sklearn.model_selection import GridSearchCV
+import string
+import re
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+import os
+import pandas as pd
+import numpy as np
+import nltk
+from sklearn.svm import SVC
+import matplotlib.pyplot as plt
+
+from sklearn.pipeline import Pipeline
+
+from sklearn import metrics
+from sklearn.feature_extraction.text import TfidfVectorizer
+import seaborn as sns
+
+
+def svc_param_selection(X, y, nfolds):
+    cs = [0.001, 0.01, 0.1, 1, 2, 5, 10, 15, 20]
+    gammas = [0.001, 0.01, 0.1, 1, 'auto']
+    kernels = ['linear', 'poly', 'rbf', 'sigmoid']
+
+    param_grid = {'C': cs,
+                  'gamma': gammas,
+                  'kernel': kernels
+                  }
+    grid_search = GridSearchCV(SVC(probability=True, C=cs, shrinking=True, gamma=gammas,
+                                   kernel=kernels, class_weight='balanced'),
+                               param_grid,
+                               cv=nfolds, iid=False, n_jobs=-1)
+    # Transfor text in features
+    vectorizer = TfidfVectorizer()
+    X = vectorizer.fit_transform(X)
+    grid_search.fit(X, y)
+    print('Migliori risultati:')
+    print(grid_search.best_params_)
+    print('Valore massimo della cross validation')
+    print(np.max(grid_search.cv_results_['mean_test_score']))
+    # print(grid_search.cv_results_['mean_test_score'])
+    print('Massimo valore dello stimator')
+    print(grid_search.best_estimator_)
+    # print(grid_search.error_score)
+    print(f'Risultato di Accuracy ottenuto da {nfolds} folders')
+    print(grid_search.best_score_)
+
+    return grid_search.best_params_
+
+
+def cleanText(text):
+    """
+    A function to clean the text, remove stopwords, lemmatize and stemming,
+    :param text: the text we want to clean as single sentence
+    :return: the string cleaned
+    """
+    try:
+        stop_words = set(stopwords.words('english'))
+        porter = PorterStemmer()
+        lemman = LancasterStemmer()
+        translator = str.maketrans('', '', string.punctuation)
+        text = re.sub(r'http\S+', '', text)
+        text = text.translate(translator)
+        return ' '.join([lemman.stem(porter.stem(w)) for w in text.split() if w not in stop_words])
+
+    except:
+        return ''
+
+def plot_histogram(dataset, y_label, palette='Blues'):
+
+    sns.set_context('paper')
+
+    # create plot
+    sns.countplot(x=y_label, data=dataset, palette=palette)
+    plt.title('Distribuzione delle classi sul dataset originale')
+    plt.show()
+
+if __name__ == '__main__':
+    # Create spam classifier
+    directory_spam_bow = os.path.join('spam_data', 'spam.csv')
+    bow = pd.read_csv(directory_spam_bow, encoding='latin-1')
+    bow = bow[['v1', 'v2']]
+    bow.columns = ['label', 'body']
+    print(bow)
+    print(len(bow))
+
+    # Balance classes
+    # bow = bow.groupby('label')
+    # bow = bow.apply(lambda x: x.sample(bow.size().min()).reset_index(drop=True))
+
+    # plot_histogram(bow, 'label', palette='Blues')
+
+    x = [cleanText(text) for text in bow['body']]
+    y = [label for label in bow['label']]
+
+    x_train, x_test, y_train, y_test = train_test_split(
+        x, y,
+        test_size=0.33,
+        random_state=0)
+
+    svc_param_selection(x, y, nfolds=5)
---
+++ main.py
@@ -0,0 +1,192 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import glob
+import pandas as pd
+from nltk.stem.lancaster import LancasterStemmer
+from nltk.corpus import stopwords
+from nltk.stem.porter import *
+import numpy as np
+import string
+import re
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.pipeline import Pipeline
+from sklearn.svm import SVC
+import nltk
+
+from handle_data import read_json
+
+
+def readjsons(folder, query):
+    """
+    Read more json in one time and return the content
+    :param folder: folder name
+    :param query: query string name
+    :return: data
+    """
+    files = glob.glob(os.path.join(os.getcwd(), 'reddit_data', folder, '*.json'))
+    files = [f for f in files if query in f]
+    data = []
+    for json_file in files:
+        data += (read_json(json_file))
+    return data
+
+
+svm_classifier = Pipeline([
+        ('feature_vect', TfidfVectorizer(strip_accents='unicode',
+                                         tokenizer=nltk.word_tokenize,
+                                         stop_words='english',
+                                         decode_error='ignore',
+                                         analyzer='word',
+                                         norm='l2',
+                                         use_idf=True,
+                                         ngram_range=(1, 2)
+                                         )),
+        ('clf', SVC(probability=True,
+                    C=2,
+                    shrinking=True,
+                    kernel='linear',
+                    gamma=0.001,
+                    class_weight='balanced'
+                    ))
+])
+
+
+def cleanText(text):
+    """
+    A function to clean the text, remove stopwords, lemmatize and stemming,
+    :param text: the text we want to clean as single sentence
+    :return: the string cleaned
+    """
+    try:
+        stop_words = set(stopwords.words('english'))
+        porter = PorterStemmer()
+        lemman = LancasterStemmer()
+        translator = str.maketrans('', '', string.punctuation)
+        text = re.sub(r'http\S+', '', text)
+        text = text.translate(translator)
+        return ' '.join([lemman.stem(porter.stem(w)) for w in text.split() if w not in stop_words])
+
+    except:
+        return ''
+
+
+def predict_spam(data, classifier, type_data):
+    """
+    It predict spam
+    :param data: the data where to clear the spam
+    :return: the ham messages dataframe
+    """
+
+    ham_messages = []
+    if type_data == 'telegram':
+        # print(data)
+        for index, message in data.iterrows():
+            pred = classifier.predict(np.array([cleanText(message['body'])]))
+            if pred != 'spam':
+                ham_messages.append(message)
+        print('\nI messaggi HAM sono: {}\ni messaggi SPAM sono: {}'.format(len(ham_messages), len(data)-len(ham_messages)))
+
+        return pd.DataFrame({'id': [d.id for d in ham_messages], 'body': [d.body for d in ham_messages],
+                             'date': [d.date for d in ham_messages]})
+    else:
+        for message in data:
+            pred = classifier.predict(np.array([cleanText(message['body'])]))
+            if pred != 'spam':
+                ham_messages.append(message)
+
+        print('\nI messaggi HAM sono: {}\ni messaggi SPAM sono: {}'.format(len(ham_messages),
+                                                                           len(data) - len(ham_messages)))
+
+        return pd.DataFrame({'subreddit': [d['subreddit'] for d in ham_messages],
+                             'body': [d['body'] for d in ham_messages],
+                             'created_utc': [d['created_utc'] for d in ham_messages],
+                             'author': [d['author'] for d in ham_messages]})
+
+
+def define_spam_classifier(directory, classifier):
+    """
+    Function to build the spam classifier
+    :param directory: directory where the spam csv is located
+    :return: svm classifier to detect spam messages
+    """
+    bow = pd.read_csv(directory, encoding='latin-1')
+    bow = bow[['v1', 'v2']]
+    bow.columns = [['label', 'body']]
+
+    x = [text[0] for i, text in bow['body'].iterrows()]
+    y = [label[0] for i, label in bow['label'].iterrows()]
+
+    x_train, x_test, y_train, y_test = train_test_split(
+        x, y,
+        test_size=0.33,
+        random_state=33)
+
+    classifier.fit(x_train, y_train)
+    preds = classifier.predict(x_test)
+    print('\n------------------------- Spam Model metrics ------------------------\n'
+          'Number of training bow: {}\n Number of test bow: {}\n\nThe accuracy of the SVM model is: {}%'.format
+          (len(x_train), len(x_test), accuracy_score(y_test, preds) * 100), '\n\n',
+          classification_report(y_test, preds))
+
+    return classifier
+
+
+def clean_spam_telegram(names, rangedate_from, rangedate_to, spam_classifier, spammer):
+    """
+    Function to clean spam from telegram messages
+    :param names: group names
+    :param rangedate_from: from date
+    :param rangedate_to: to date
+    :param spam_classifier: classifier to detect spam
+    :param spammer: string obj
+    :return: None
+    """
+    for nome in names:
+        data = pd.read_csv(
+            os.path.join(os.getcwd(), 'telegram_data',
+                         nome + '_' + rangedate_from + '_to_' + rangedate_to + '_TELEGRAM.csv'),
+            sep=None, engine='python')
+
+        data.columns = ['0', 'id', 'body', 'to_id', 'date']
+        data = data.iloc[:, 1:]
+        df = predict_spam(data, spam_classifier, type_data=spammer)
+
+        df.to_csv(os.path.join(os.getcwd(), 'telegram_data', 'clean_data', nome+'_cleanedData_'
+                               + rangedate_from + '_' + rangedate_to + '.csv'))
+        print('File salvato correttamente nella cartella "CLEANED_DATA"')
+
+
+def clean_spam_reddit(query, folder):
+    """
+    Function to clean spam from reddit messages
+    :param reddit_queries: query names
+    :param spam_classifier: classifier
+    :param spammer: string obj
+    :param folder: folder name str
+    :return: None
+    """
+
+    data = readjsons(folder, query=query)
+
+    return data
+
+if __name__ == '__main__':
+    # Select spammer
+    spammer = 'reddit' # reddit
+
+    # If reddit spam messages set folder
+    reddit_folder = 'libra_folder'
+    reddit_queries = 'Facebook'
+
+    # If telegram spam messages to consider
+    rangedate_from, rangedate_to = '2019-07-01', '2019-11-18'
+
+    data = clean_spam_reddit(reddit_queries, reddit_folder)
+    # print(data)
+    print(data[0])
+    for item in data:
+        if len(item['all_awardings']) > 0:
+            print(item['all_awardings'])
